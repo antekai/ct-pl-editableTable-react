e343ea94e307c40e57ab7c1967b5012a32f042e1
Andreas Teneketzoglou <antekai00@gmail.com>
2018-08-30 00:07:10 +0200

    delete previous ISO-date and rename germanDate attribute to date

--- src/utils/preprocessData.js
+++ src/utils/preprocessData.js
@@ -13,10 +13,14 @@ const moment = require("moment");
 
 // Create time(HH:mm) date(DD.MM.YYYY) attributes with required format
 // manual test that parsing was successful
-export const dataWithTimeDate = dropUnusedAttributes.map(obj => {
+const dataWithTimeDate = dropUnusedAttributes.map(obj => {
   obj.time = moment(obj.date).format("HH:mm");
   obj.germanDate = moment(obj.date).format("DD.MM.YYYY");
   return obj;
 });
+// delete the previous ISO date attribute and rename the "germanDate" attribute to "date"
+const dataWithTimeDateClean = dataWithTimeDate
+  .map(({ date, ...keepRest }) => keepRest)
+  .map(({ germanDate: date, ...keepRest }) => ({ date, ...keepRest }));
 
-export const preprocessData = dataWithTime;
+export const preprocessData = dataWithTimeDateClean;
