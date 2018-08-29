06a21476b9575695265b44609fbf791f68ced554
Andreas Teneketzoglou <antekai00@gmail.com>
2018-08-29 21:09:21 +0200

    add margin

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import React from "react";
 import EditableTable from "./EditableTable/EditableTable";
+import "./App.css";
 
-export const App = () => <EditableTable />;
+export const App = () => (
+  <div className="margin2">
+    <EditableTable />
+  </div>
+);
