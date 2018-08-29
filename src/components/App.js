c23c5288864a9d8b405cd7c6ab464bcf9d79fb4a
Andreas Teneketzoglou <antekai00@gmail.com>
2018-08-29 20:46:22 +0200

    structure app

---
+++ src/components/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Table from "./Table/Table";
+
+export const App = () => <Table />;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Table extends React.Component {
+  render() {
+    return <div>Table</div>;
+  }
+}
