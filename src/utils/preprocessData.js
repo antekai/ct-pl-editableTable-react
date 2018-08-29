975a13b66f247ac510c454530dff1a4ac76fa1b6
Andreas Teneketzoglou <antekai00@gmail.com>
2018-08-29 22:46:59 +0200

    drop unused attributes from raw data

--- src/utils/preprocessData.js
+++ src/utils/preprocessData.js
@@ -1,3 +1,11 @@
 import * as rawData from "./fixtures/data.json";
 
-export const preprocessData = rawData;
+// Drop attributes that we do not need
+// check other methods
+// https://stackoverflow.com/questions/10024866/remove-object-from-array-using-javascript
+
+const dropUnusedAttributes = rawData.map(
+  ({ a, campaignid, userid, frienddomainid, ...keepRest }) => keepRest
+);
+
+export const preprocessData = dropUnusedAttributes;
