b8dc1c0c391adff2e88865b81619ad0e50d84909
Andreas Teneketzoglou <antekai00@gmail.com>
2018-08-31 16:08:56 +0200

    extract antColumns from EditableTable.js

--- src/EditableTable/EditableTable.js
+++ src/EditableTable/EditableTable.js
@@ -4,6 +4,7 @@ import { EditableFormRow, EditableContext } from "./EditableFormRow";
 import EditableCell from "./EditableCell";
 import { plCleanData } from "../data/plCleanData";
 import { antData } from "../data/antData";
+import { antColumns } from "./antColumns";
 
 console.log(antData);
 //test data preprocess
@@ -14,30 +15,7 @@ export default class EditableTable extends React.Component {
     super(props);
     this.state = { data: antData, editingKey: "" };
     this.columns = [
-      {
-        title: "name",
-        dataIndex: "name",
-        width: "25%",
-        editable: true
-      },
-      {
-        title: "age",
-        dataIndex: "age",
-        width: "15%",
-        editable: true
-      },
-      {
-        title: "address",
-        dataIndex: "address",
-        width: "40%",
-        editable: true
-      },
-      {
-        title: "camp_cpc",
-        dataIndex: "camp_cpc",
-        width: "10%",
-        editable: true
-      },
+      ...antColumns,
       {
         title: "operation",
         dataIndex: "operation",
@@ -73,6 +51,66 @@ export default class EditableTable extends React.Component {
         }
       }
     ];
+    // this.columns = [
+    //   {
+    //     title: "name",
+    //     dataIndex: "name",
+    //     width: "25%",
+    //     editable: true
+    //   },
+    //   {
+    //     title: "age",
+    //     dataIndex: "age",
+    //     width: "15%",
+    //     editable: true
+    //   },
+    //   {
+    //     title: "address",
+    //     dataIndex: "address",
+    //     width: "40%",
+    //     editable: true
+    //   },
+    //   {
+    //     title: "camp_cpc",
+    //     dataIndex: "camp_cpc",
+    //     width: "10%",
+    //     editable: true
+    //   },
+    //   {
+    //     title: "operation",
+    //     dataIndex: "operation",
+    //     render: (text, record) => {
+    //       const editable = this.isEditing(record);
+    //       return (
+    //         <div>
+    //           {editable ? (
+    //             <span>
+    //               <EditableContext.Consumer>
+    //                 {form => (
+    //                   <a
+    //                     href="javascript:;"
+    //                     onClick={() => this.save(form, record.key)}
+    //                     style={{ marginRight: 8 }}
+    //                   >
+    //                     Save
+    //                   </a>
+    //                 )}
+    //               </EditableContext.Consumer>
+    //               <Popconfirm
+    //                 title="Sure to cancel?"
+    //                 onConfirm={() => this.cancel(record.key)}
+    //               >
+    //                 <a>Cancel</a>
+    //               </Popconfirm>
+    //             </span>
+    //           ) : (
+    //             <a onClick={() => this.edit(record.key)}>Edit</a>
+    //           )}
+    //         </div>
+    //       );
+    //     }
+    //   }
+    // ];
   }
 
   isEditing = record => {
